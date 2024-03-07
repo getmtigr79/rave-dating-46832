56358a5f6ec220a977ea8fc6e2c03d36a3a73f89
Jill Mudgette <peoplewithpassions@gmail.com>
2024-03-06 22:22:02 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -5,7 +5,7 @@ export const globalOptions = {
   "screenOptions": {
     "headerShown": false
   },
-  "initialRoute": "signup-2"
+  "initialRoute": "login-2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
